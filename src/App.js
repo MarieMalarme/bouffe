60269c98b2f0bd26bc8c2adab36cf088311a28fc
Marie Malarme <malarmemarie@gmail.com>
2020-05-16 23:08:39 +0100

    Added data for recipes.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,33 @@ import { Component, Div } from './lib/design.js'
 
 import './App.css'
 
+const recipes = [
+  {
+    title: 'Turkey honey mustard',
+    ingredients: [
+      { ingredient: 'Turkey leg' },
+      { ingredient: 'Plantains' },
+      { ingredient: 'Mustard', measure: '4 tbsp' },
+      { ingredient: 'Soy sauce', measure: '2 tbsp' },
+    ],
+    steps: [
+      'Preheat the oven',
+      'Mix the sauce',
+      'Glaze the leg with the sauce',
+      'Put 10cl of water in the plate',
+    ],
+  },
+  {
+    title: 'Lamb with spices',
+    ingredients: ['Lamb chops', 'Plantains'],
+    steps: [
+      'Preheat the oven',
+      'Mix the spices',
+      'Glaze the chops with the mixture',
+    ],
+  },
+]
+
 const App = () => {
   return (
     <Div pa100>
