a008a502eb241df117c19cf3de6091584809992c
Marie Malarme <malarmemarie@gmail.com>
2020-06-13 00:08:35 +0100

    Added real data in the JSON file.

--- server/routes/test.js
+++ server/routes/test.js
@@ -15,26 +15,3 @@ router.get('/', (req, res, next) => {
 })
 
 module.exports = router
-
-// const express = require('express')
-// const fs = require('fs')
-// const util = require('util')
-// const path = require('path')
-
-// const router = express.Router()
-// const readFile = util.promisify(fs.readFile)
-// const writeFile = util.promisify(fs.writeFile)
-
-// const filePath = path.join(__dirname, '../data/test.json')
-
-// router.get('/', (req, res, next) => {
-//   readFile(filePath, 'utf8')
-//     .then((data) => {
-//       const test = JSON.parse(data)
-//       console.log(test)
-//       response.json(test)
-//     })
-//     .catch(next)
-// })
-
-// module.exports = router
