217af13af414beace5d7220c3cc1513c8927aabb
Marie Malarme <malarmemarie@gmail.com>
2020-06-13 01:30:00 +0100

    Handled 'POST' to add a new recipe and write in the JSON file.

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -16,6 +17,9 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
@@ -24,9 +28,41 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+// app.use('/users', usersRouter)
 
 app.use('/test', test)
+
+app.use((request, response, next) => {
+  console.log(request)
+  response.header('Access-Control-Allow-Origin', request.headers.origin)
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  response.header('Access-Control-Allow-Credentials', 'true')
+  response.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+  next()
+})
+
+app.use((request, response, next) => {
+  console.log('ici', request, response)
+  if (request.method !== 'POST' && request.method !== 'PUT') return next()
+  let accumulator = ''
+
+  request.on('data', (data) => {
+    accumulator += data
+    console.log(data)
+  })
+  request.on('end', () => {
+    try {
+      request.body = JSON.parse(accumulator)
+      console.log(request.body)
+      next()
+    } catch (err) {
+      next(err)
+    }
+  })
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/test.js
+++ server/routes/test.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const util = require('util')
 const path = require('path')
 const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
 
 const filePath = path.join(__dirname, './test.json')
 
@@ -11,6 +12,18 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   readFile(filePath, 'utf8')
     .then((data) => res.json(JSON.parse(data)))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  readFile(filePath, 'utf8')
+    .then(JSON.parse)
+    .then(async (data) => {
+      const recipes = [...data, req.body]
+      const content = JSON.stringify(recipes, null, 2)
+      await writeFile(filePath, content, 'utf-8')
+      res.json(recipes)
+    })
     .catch(next)
 })
 
