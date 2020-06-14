2c41fcf7d9d45b5c5120d53bd0e8c9174063f8ba
Marie Malarme <malarmemarie@gmail.com>
2020-06-14 19:11:37 +0100

    Renamed `open` to `modale` + moved the `useState` at the root of `Home`.

--- client/src/Controls.js
+++ client/src/Controls.js
@@ -14,10 +14,10 @@ export const Controls = ({
   setFilters,
   recipes,
   setRecipes,
+  modale,
+  setModale,
 }) => {
-  const [open, setOpen] = useState({ clicked: false, hovered: false })
-
-  body.style.overflow = open ? 'hidden' : 'auto'
+  body.style.overflow = modale ? 'hidden' : 'auto'
 
   const filter = (target) =>
     filters.includes(target)
@@ -31,12 +31,12 @@ export const Controls = ({
         recipes={recipes}
         setData={setData}
         data={data}
-        open={open}
-        setOpen={setOpen}
+        modale={modale}
+        setModale={setModale}
       />
       <Modale
-        open={open}
-        setOpen={setOpen}
+        modale={modale}
+        setModale={setModale}
         recipes={recipes}
         setRecipes={setRecipes}
         data={data}
@@ -48,30 +48,30 @@ export const Controls = ({
 
 const Bar = Component.flex.justifyBetween.sticky.pb60.pt80.t0.mb20.w100p.div()
 
-const New = ({ recipes, data, setData, open, setOpen }) => {
-  const { clicked } = open
+const New = ({ recipes, data, setData, modale, setModale }) => {
+  const { editing } = modale
 
   return (
     <Button
-      bgGrey9={clicked}
-      shadowOut={clicked}
-      onMouseEnter={() => !clicked && setOpen({ hovered: true })}
-      onMouseLeave={() => !clicked && setOpen({ hovered: false })}
+      bgGrey9={editing}
+      shadowOut={editing}
+      onMouseEnter={() => !editing && setModale({ hovering: true })}
+      onMouseLeave={() => !editing && setModale({ hovering: false })}
       onClick={() => {
         const emptyData = !Object.values(data).length
         if (emptyData) {
           const id = Math.max(...recipes.map((r) => r.id)) + 1
           setData({ id })
         }
-        setOpen({ clicked: !clicked, hovered: false })
+        setModale({ editing: !editing, hovering: false })
       }}
     >
       <Plus
         anim
         width={20}
         strokeWidth={7}
-        stroke={clicked ? 'black' : 'white'}
-        style={{ transform: `rotate(${clicked ? 45 : 0}deg)` }}
+        stroke={editing ? 'black' : 'white'}
+        style={{ transform: `rotate(${editing ? 45 : 0}deg)` }}
       />
     </Button>
   )
